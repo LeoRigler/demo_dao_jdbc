80bf85c8a620a60fbf18199ebe6bc8d2d37f1867
Leonardo Rigler <leorigler98@gmail.com>
2023-03-20 21:05:41 -0300

    implementando findByDepartment

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -111,6 +114,51 @@ public class SellerDaoJDBC implements SellerDao{
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null; 
+		ResultSet rs = null; 
+		try { 
+			st = conn.prepareStatement(
+					" SELECT seller.*,department.Name as DepName "
+					+ " FROM seller INNER JOIN department "
+					+ " ON seller.DepartmentId = department.Id "
+					+ " WHERE DepartmentId = ? "
+					+ "ORDER BY Name");
+			
+			st.setInt(1, department.getId()); //Como quero achar o ID, será utilizado na pesquisa o ID. 
+			
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>(); 
+			Map<Integer, Department> map = new HashMap<>(); //Criei um hashmap para o código não criar um departamento novo pra cada resultado
+			
+			//Será utilizado um while pois a consulta pode retornar mais de um valor; 
+			while(rs.next()) { 
+				
+				Department dep = map.get(rs.getInt("DepartmentId")); 
+				//Aqui, estamos pegando no Map um get pra pegar do rs citado acima, após isso estamos pedindo o ID dele e o que foi colocado em String, é o nome da coluna
+				
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj); 
+			}
+			return list; 	
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage()); 
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}	
 	}
 	
 	
